760386786db4f6e35de47f3935fc27ee7f364d92
lyg509 <dydrjs252@naver.com>
2022-11-07 15:00:04 +0900

    1.환경 설정 2.DB 연결 3.게시글 작성폼 생성 4.글 작성 처리 5.게시글 리스트

---
+++ src/main/java/com/lyg/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.lyg.board.controller;
+
+import com.lyg.board.entity.Board;
+import com.lyg.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+        return"";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list",boardService.boardList());
+        return "boardlist";
+    }
+}
