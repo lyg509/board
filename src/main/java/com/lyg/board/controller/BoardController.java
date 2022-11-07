dcbd5e534b8f0336ed66e765b74170744b72c306
lyg509 <dydrjs252@naver.com>
2022-11-07 17:12:45 +0900

    게시글 상페 페이지 및 게시글 삭제 기능 추가

--- src/main/java/com/lyg/board/controller/BoardController.java
+++ src/main/java/com/lyg/board/controller/BoardController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -33,4 +32,20 @@ public class BoardController {
         model.addAttribute("list",boardService.boardList());
         return "boardlist";
     }
+
+    @GetMapping("/board/view") //localhost:8091/board/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+
 }
